632680295beb8c1b30c1f306cb2c753c5683f7ca
ndiawar gueye <ndiawar888@gmail.com>
2022-03-06 20:48:22 +0000

    personne component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonneComponent } from './GestionPersonne/personne/personne.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonneComponent
   ],
   imports: [
     BrowserModule
